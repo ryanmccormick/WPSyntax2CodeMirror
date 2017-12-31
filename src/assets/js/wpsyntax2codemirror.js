ba51595c09099a259ed7ab9977cc48cc8dd202cc
Ryan McCormick <ryan@itjobsguy.com>
2017-12-31 16:20:56 -0600

    fix formatting

--- src/assets/js/wpsyntax2codemirror.js
+++ src/assets/js/wpsyntax2codemirror.js
@@ -3,36 +3,37 @@
  * Uses JQuery to replace all pre[lang] elements with a javascript CodeMirror
  * Instance.
  */
-var wpsyntax2codemirror = (function($, document, window, CodeMirror) {
-	'use strict';
+var wpsyntax2codemirror = (function ($, document, window, CodeMirror) {
+  'use strict';
 
   /**
-	 * Public interface.
+   * Public interface.
    * @type {{init: init}}
    */
-	var wp2cm = {
-		init: init
-	};
+  var wp2cm = {
+    init: init
+  };
 
-	return wp2cm;
-	///////////////
+  return wp2cm;
+
+  ///////////////
 
   /**
-	 * Main Entrypoint for converting WPSyntax Elements to CodeMirror Elements.
+   * Main Entrypoint for converting WPSyntax Elements to CodeMirror Elements.
    */
-	function init() {
+  function init() {
     if (CodeMirror) {
       convertAllWPSyntaxElementsToCodeMirror();
     } else {
       throw new Error('CodeMirror Does Not Exist');
-		}
-	}
+    }
+  }
 
   /**
-	 * Performs the conversion of all WPSyntaxElements to CodeMirrorElements.
+   * Performs the conversion of all WPSyntaxElements to CodeMirrorElements.
    */
-	function convertAllWPSyntaxElementsToCodeMirror() {
-		try {
+  function convertAllWPSyntaxElementsToCodeMirror() {
+    try {
       var wpSyntaxElements = getAllWPSyntaxElements(),
         currentMode,
         currentValue,
@@ -46,47 +47,47 @@ var wpsyntax2codemirror = (function($, document, window, CodeMirror) {
         swapWithCodeMirror(currentElement, modeConverter(currentMode), currentValue);
       }
     } catch (exception) {
-			console.error('wpsyntax2codemirror::', exception);
-		}
-	}
+      console.error('wpsyntax2codemirror::', exception);
+    }
+  }
 
   /**
-	 * Since the TypeScript mode is a specialization of the javascript mode,
-	 * TypeScript mode must be provided as 'text/typescript' not just typescript
-	 * for CodeMirror to display the correct syntax highlighting.
+   * Since the TypeScript mode is a specialization of the javascript mode,
+   * TypeScript mode must be provided as 'text/typescript' not just typescript
+   * for CodeMirror to display the correct syntax highlighting.
    * @param currentMode
    * @returns {string}
    */
-	function modeConverter(currentMode) {
-		try {
+  function modeConverter(currentMode) {
+    try {
       return currentMode === 'typescript' ? 'text/typescript' : currentMode;
     } catch (exception) {
       console.error('wpsyntax2codemirror::', exception);
-		}
-	}
+    }
+  }
 
   /**
-	 * Handles the DOM Swap of elements. Replaces the pre[lang] with
-	 * a custom code mirror element.
+   * Handles the DOM Swap of elements. Replaces the pre[lang] with
+   * a custom code mirror element.
    * @param element
    * @param mode
    * @param value
    * @returns {*}
    */
-	function swapWithCodeMirror(element, mode, value) {
-		return CodeMirror(function(elt) {
-			$(element).replaceWith(elt);
-		}, {
-			value: value,
-			mode: mode,
-			lineNumbers: true,
+  function swapWithCodeMirror(element, mode, value) {
+    return CodeMirror(function (elt) {
+      $(element).replaceWith(elt);
+    }, {
+      value: value,
+      mode: mode,
+      lineNumbers: true,
       readOnly: 'nocursor',
-			viewportMargin: Infinity
-		});
-	}
+      viewportMargin: Infinity
+    });
+  }
 
   /**
-	 * Returns a list of all target WP Syntax Elements.
+   * Returns a list of all target WP Syntax Elements.
    * @returns {*|HTMLElement}
    */
   function getAllWPSyntaxElements() {
@@ -98,6 +99,6 @@ var wpsyntax2codemirror = (function($, document, window, CodeMirror) {
 /**
  * Replace all WPSyntax Editors with CodeMirror.
  */
-$(document).ready(function() {
-	wpsyntax2codemirror.init();
+$(document).ready(function () {
+  wpsyntax2codemirror.init();
 });
